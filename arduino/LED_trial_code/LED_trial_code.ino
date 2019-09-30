65e037dfdcdc1a1cca1c156826f783aec9a045a6
Niels Weggeman <nielsweggeman@gmail.com>
2019-09-30 17:14:04 +0200

    Updated arduino, again.

--- arduino/LED_trial_code/LED_trial_code.ino
+++ arduino/LED_trial_code/LED_trial_code.ino
@@ -7,6 +7,8 @@ int i = 0;
 int vibrState = 1;
 int state = 0;
 
+char command = '0';
+
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   Serial.begin(9600); // setting baud speed for Serial (a baud is a pulse)
@@ -14,7 +16,6 @@ void setup() {
 }
 
 void loop() {
-
   // PWM takes values from 0 to 255, in our case, we want to make
   // a pulse effect, so we detect out of bounds behaviour and go to 127
 
@@ -22,28 +23,33 @@ void loop() {
 
   vibrationPattern();
 
-  Serial.println(i);
-  analogWrite(LED_PIN, i);
-  delay(10); // in each step of pwm, we vibrate for i * 0.01 seconds
+  //Serial.println(i);
 
 }
 
 void vibrationPattern() {
-  char command = Serial.read();
-  if (command == 0) {
-    i = 0;
-  } else if (command == 1) {
-    i = 255;
-  } else if (state == 2) {
-    if (vibrState == 1) i += 10;
-    if (vibrState == 2) i -= 10;
-
-    if ( i > 255 && vibrState == 1) {
-      vibrState = 2;
-      i = 255;
-    } else if (i < 0 && vibrState == 2) {
-      vibrState = 1;
-      i = 0;
-    }
+  command = char(Serial.read());
+  Serial.println(int(command));
+  if (command =='0') {
+    analogWrite(LED_PIN, 255);  
+  } else if (command == '1') {
+    analogWrite(LED_PIN, 0); 
   }
+  //  Serial.println(command);
+  //  if (command == 0) {
+  //    i = 0;
+  //  } else if (command == 1) {
+  //    i = 255;
+  //  } else if (state == 2) {
+  //    if (vibrState == 1) i += 10;
+  //    if (vibrState == 2) i -= 10;
+  //
+  //    if ( i > 255 && vibrState == 1) {
+  //      vibrState = 2;
+  //      i = 255;
+  //    } else if (i < 0 && vibrState == 2) {
+  //      vibrState = 1;
+  //      i = 0;
+  //    }
+  //  }
 }
