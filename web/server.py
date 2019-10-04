ef0979f3d8c885aa0362e2aa196fc0d4ea76fdf8
Niels Weggeman <nielsweggeman@gmail.com>
2019-10-04 09:45:38 +0200

    Web file updated

--- web/server.py
+++ web/server.py
@@ -1,5 +1,10 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret!'
+socketio =  SocketIO(app)
 
 @app.route('/')
 def hello_world():
