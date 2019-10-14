2c84cd2fe21be38ea48cacfaacb6421d2e488746
Niels Weggeman <nielsweggeman@gmail.com>
2019-10-14 14:45:47 +0200

    Experimental SGO file

--- py/subscribe_gatt_orientation.py
+++ py/subscribe_gatt_orientation.py
@@ -7,6 +7,7 @@ import os  # To access environment variables
 from dotenv import load_dotenv  # To load the environment variables from the .env file
 from time import sleep
 
+from flask import Flask, request, render_template
 from flask_socketio import SocketIO, emit, send
 
 
@@ -26,6 +27,15 @@ GATT_CHARACTERISTIC_ORIENTATION = "02118833-4455-6677-8899-aabbccddeeff"
 
 # Many devices, e.g. Fitbit, use random addressing, this is required to connect.
 ADDRESS_TYPE = pygatt.BLEAddressType.random
+
+# ==== ==== ===== == =====  Web server
+
+app = Flask(__name__)
+
+app.config['SECRET KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+# ==== ==== ===== == =====  Initializing Values
 
 cur_loc = (-666, -666, -666)
 degreesRotated = 0
@@ -175,26 +185,92 @@ def read_characteristic(device, characteristic_id):
 def keyboard_interrupt_handler(signal_num, frame):
     """Make sure we close our program properly"""
     print("Exiting...".format(signal_num))
-    left_wheel.unsubscribe(GATT_CHARACTERISTIC_ORIENTATION)
+    wheel.unsubscribe(GATT_CHARACTERISTIC_ORIENTATION)
     exit(0)
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/distance')
+def distance():
+    distBar = distVal
+    return render_template('distanceVis.html', distBar=distBar)
+
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))
+    emit('json', json, broadcast=True)
+
+@socketio.on('distance')
+def handle_distance(json):
+    print( float(json['distance']))
+
+
+
+
+def handle_distance_data(handle, value_bytes):
+    #handle -- integer, characteristic read handle the data was received on
+    #value_bytes -- bytearray, the data returned in the notification
+    print("Received data: %s (handle %d)" % (str(value_bytes), handle))
+    values = [float(x) for x in value_bytes.decode('utf-8').split(",")]
+    global distVal
+    distVal = (float(value_bytes))
+    print(distVal)
+
+    #print(distVal)
+    try:
+       socketio.emit('distance', '{"distance": "%s"}' % str(distVal), broadcast=True)
+    except:
+       print("No socket?")
+    return distVal
+
+
+def discover_characteristic(device):
+    #List characteristics of a device
+    for uuid in device.discover_characteristics().keys():
+        try:
+            print("Read UUID" + str(uuid) + "   " + str(device.char_read(uuid)))
+        except:
+            print("Something wrong with " + str(uuid))
+
+
+def read_characteristic(device, characteristic_id):
+    #Read a characteristic
+    return device.char_read(characteristic_id)
+
+
+def keyboard_interrupt_handler(signal_num, frame):
+    #Make sure we close our program properly
+    print("Exiting...".format(signal_num))
+    wheel.unsubscribe(GATT_CHARACTERISTIC_DISTANCE)
+    exit(0)
 
 # Instantiate a thing with its credential, then read its properties from the DCD Hub
 #my_thing = Thing(thing_id=THING_ID, token=THING_TOKEN)
 #my_thing.read()
 
-# Start a BLE adapter
-bleAdapter = pygatt.GATTToolBackend()
-bleAdapter.start()
+def connect_bluetooth():
+    print("Starting Bluetooth...")
+    # Start a BLE adapter
+    bleAdapter = pygatt.GATTToolBackend()
+    bleAdapter.start()
 
-# Use the BLE adapter to connect to our device
-left_wheel = bleAdapter.connect(BLUETOOTH_DEVICE_MAC, address_type=ADDRESS_TYPE)
+    print("connecting to Bluetooth device...")
+    # Use the BLE adapter to connect to our device
+    wheel = bleAdapter.connect(BLUETOOTH_DEVICE_MAC, address_type=ADDRESS_TYPE)
 
-# Subscribe to the GATT service
-left_wheel.subscribe(GATT_CHARACTERISTIC_ORIENTATION, callback=handle_orientation_data)
+    # Subscribe to the GATT service
+    wheel.subscribe(GATT_CHARACTERISTIC_ORIENTATION,
+            callback=handle_orientation_data)
 
 # Register our Keyboard handler to exit
 signal.signal(signal.SIGINT, keyboard_interrupt_handler)
+
+connect_bluetooth()
+
+if __name__ == '__main__':
+    socketio.run(app, host = '0.0.0.0')
 
 while True :
     sleep(5)
